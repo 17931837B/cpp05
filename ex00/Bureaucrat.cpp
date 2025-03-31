026eee5fbfb678f67ca4edc1976c07a6534ac4d4
tomoya baba <tomoya.bb17931837@gmail.com>
2025-04-01 00:28:40 +0900

    ex00 done

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -58,12 +58,14 @@ void Bureaucrat::decrementGrade()
 		grade_++;
 }
 
-const char* Bureaucrat::GradeTooHighException::what() const throw() {
-    return "Grade too high";
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade too high");
 }
 
-const char* Bureaucrat::GradeTooLowException::what() const throw() {
-    return "Grade too low";
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade too low");
 }
 
 std::ostream &operator<<(std::ostream &os, Bureaucrat const &other)
