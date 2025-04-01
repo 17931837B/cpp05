8a5ec82aeb2be91f7ca8d3a39c833e5baf406812
tomoya baba <tomoya.bb17931837@gmail.com>
2025-04-01 15:48:01 +0900

    ex01 doing

---
+++ ex01/Bureaucrat.hpp
@@ -0,0 +1,38 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+
+class Bureaucrat
+{
+	private:
+		std::string	name_;
+		int			grade_;
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string name, int grade);
+		~Bureaucrat();
+		Bureaucrat(const Bureaucrat &other);
+		Bureaucrat	&operator=(const Bureaucrat &other);
+		std::string	getName() const;
+		int			getGrade() const;
+		void		incrementGrade();
+		void		decrementGrade();
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+};
+
+std::ostream &operator<<(std::ostream & os, Bureaucrat const &other);
+std::ostream	&operator<<(std::ostream &res, Bureaucrat const &fixed);
+
+#endif
---
+++ ex01/Form.hpp
@@ -0,0 +1,12 @@
+#ifndef FORM.HPP
+# define FORM.HPP
+
+#include <iostream>
+
+class Form
+{
+	std::string	name_;
+	bool		is_signed_;
+	int			sign_grade_;
+	int			execute_grade_;
+}
